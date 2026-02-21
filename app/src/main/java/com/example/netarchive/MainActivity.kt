4d10f1549f27603f4e1e3bfd26b58c45456b473f
Max <rcmaksim@gmail.com>
2026-02-22 00:06:40 +0300

    added navigation

--- app/src/main/java/com/example/netarchive/MainActivity.kt
+++ app/src/main/java/com/example/netarchive/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.netarchive.ui.navigation.AppNavHost
 import com.example.netarchive.ui.theme.NetArchiveTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +24,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    AppNavHost()
+                    val navController = rememberNavController()
+                    AppNavHost(navController = navController)
                 }
             }
         }
