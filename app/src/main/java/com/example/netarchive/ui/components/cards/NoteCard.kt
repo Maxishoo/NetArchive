6ab6d0ca465a98597a01ad684be183e9b1e70de6
Alena-Sergeeva <sergeevanat71@icloud.com>
2026-03-13 14:23:49 +0300

    add icons in NoteCard

--- app/src/main/java/com/example/netarchive/ui/components/cards/NoteCard.kt
+++ app/src/main/java/com/example/netarchive/ui/components/cards/NoteCard.kt
@@ -4,36 +4,45 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccessTime
+import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.netarchive.R
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.netarchive.domain.model.Note
 import java.text.SimpleDateFormat
 import java.util.*
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.netarchive.ui.theme.NetArchiveTheme
 
 @Composable
 fun NoteCard(
     note: Note,
     modifier: Modifier = Modifier,
-    isEditMode: Boolean = false,  // <-- Добавь этот параметр
-    onNoteClick: () -> Unit = {},  // <-- Добавь
+    isEditMode: Boolean = false,
+    onNoteClick: () -> Unit = {},
     onDeleteClick: () -> Unit = {}
 ) {
     val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
     val dateString = dateFormat.format(Date(note.date))
 
+    val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+    val timeString = timeFormat.format(Date(note.date))
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onNoteClick() },  // <-- Тап по заметке
+            .clickable { onNoteClick() },
         shape = RoundedCornerShape(12.dp),
         color = MaterialTheme.colorScheme.surfaceVariant
-    ) {
+    )
+    {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -41,7 +50,6 @@ fun NoteCard(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            // Текст и дата
             Column(modifier = Modifier.weight(1f)) {
                 Text(
                     text = note.text,
@@ -50,20 +58,35 @@ fun NoteCard(
                     maxLines = 3,
                     overflow = TextOverflow.Ellipsis
                 )
-                Text(
-                    text = dateString,
-                    style = MaterialTheme.typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f),
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-            }
-
-            if (isEditMode) {
-                IconButton(onClick = onDeleteClick) {
+                Row(
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Icon(
-                        imageVector = Icons.Default.Delete,
-                        contentDescription = "Удалить",
-                        tint = MaterialTheme.colorScheme.error
+                        imageVector = Icons.Default.CalendarToday,
+                        contentDescription = stringResource(R.string.content_description_date),
+                        modifier = Modifier.size(16.dp),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f)
+                    )
+
+                    Text(
+                        text = dateString,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f),
+                        modifier = Modifier.padding(start = 4.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Icon(
+                        imageVector = Icons.Default.AccessTime,
+                        contentDescription = stringResource(R.string.content_description_clock),
+                        modifier = Modifier.size(16.dp),
+                        tint = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.6f)
+                    )
+                    Text(
+                        text = timeString,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.7f),
+                        modifier = Modifier.padding(start = 4.dp)
                     )
                 }
             }
@@ -72,3 +95,20 @@ fun NoteCard(
 }
 
 
+@Preview(showBackground = true)
+@Composable
+fun NoteCardPreview() {
+    NetArchiveTheme {
+        NoteCard(
+            note = Note(
+                id = 1,
+                text = "Это пример заметки с длинным текстом, котоkjhkjhhhkgjhgvjhgjhgjhgjhhjghjgjhgyuрыkjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjй может занимать несколько строк и будет обрезан с многоточием",
+                contactId = 8,
+                date = System.currentTimeMillis(),
+            ),
+            isEditMode = false,
+            onNoteClick = {},
+            onDeleteClick = {}
+        )
+    }
+}
