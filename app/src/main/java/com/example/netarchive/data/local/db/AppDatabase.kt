d9839c702eb210a3224485150ce08e6218a0cacb
Alena-Sergeeva <sergeevanat71@icloud.com>
2026-04-05 15:59:21 +0300

    merge branch 'develop' into add_reminder_screen

--- app/src/main/java/com/example/netarchive/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/netarchive/data/local/db/AppDatabase.kt
@@ -14,9 +14,11 @@ import com.example.netarchive.data.local.db.entity.ContactEntity
 import com.example.netarchive.data.local.db.entity.NoteEntity
 
 import com.example.netarchive.data.local.db.entity.ProfileEntity
+import com.example.netarchive.data.local.db.entity.ReminderEntity
+
 //повысила версию после добавления списка напоминаний была 6-я
 @Database(
-    entities = [(ContactEntity::class), (NoteEntity::class), (CategoryEntity::class), (ContactCategoryCrossRef::class), (ProfileEntity::class)],
+    entities = [(ContactEntity::class), (NoteEntity::class), (ReminderEntity::class), (CategoryEntity::class), (ContactCategoryCrossRef::class), (ProfileEntity::class)],
     version = 7,
     exportSchema = false
 )
